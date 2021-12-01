b53f8dc7cdd844b601c3db3ad19138acab5e2835
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-12-01 11:21:21 +0500

    animate

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const questionsBtn = document.querySelectorAll('.questions__block');
+for(let i = 0; i < questionsBtn.length; i++) {
+    questionsBtn[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+    });
+};
+
+$(window).scroll(function() {
+    let st = $(this).scrollTop();
+
+    $(".banner__right .img img").css({
+        "transform" : "translate(0px, " + st/25 + "px"
+    });
+    $(".banner__right .img span").css({
+        "transform" : "translate(0px, -" + st/25 + "px"
+    });
+    $(".animate_h1").css({
+        "transform" : "translate(" + -st/10 + "px, " + "0px"
+    });
+    $(".animate_p").css({
+        "transform" : "translate(" + -st/20 + "px, " + "0px"
+    });
+    $(".animate_btn").css({
+        "transform" : "translate(" + -st/30 + "px, " + "0px"
+    });
+});
